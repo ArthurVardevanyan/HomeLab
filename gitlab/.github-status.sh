de4c6a45a24e666d9cbf592cf95ac713dcc2995a
Arthur <arthur@arthurvardevanyan.com>
2021-11-05 18:01:19 -0400

    GitLab CI/CD Sub Folder

---
+++ gitlab/.github-status.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+URL="https://${GITHUB_TOKEN}:x-oauth-basic@api.github.com/repos/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}"
+JSON_STRING='{"state":"'"${STATUS}"'","description":"Pipeline '"${DESCRIPTION}"'", "context":"GitLab CI/CD"}'
+
+curl -X POST -H "Accept: application/vnd.github.v3+json" ${URL}/statuses/${CI_COMMIT_SHA} -d "${JSON_STRING}"
