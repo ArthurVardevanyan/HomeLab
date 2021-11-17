b4b9c79343876f371f4d16f0e097aca12ab945c4
Arthur <arthur@arthurvardevanyan.com>
2021-11-17 17:54:25 -0500

    Image Version Updates

--- kubernetes/cert-manager/cert-manager-deployment.sh
+++ kubernetes/cert-manager/cert-manager-deployment.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.6.0/cert-manager.yaml
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.6.1/cert-manager.yaml
