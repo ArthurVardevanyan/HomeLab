5823df096e92a56224ee41803edb28c5a32683aa
Arthur <arthur@arthurvardevanyan.com>
2022-10-19 21:02:15 -0400

    Cert Manager Network Policy & Upgrade

--- kubernetes/cert-manager/components/yaml/crds.sh
+++ kubernetes/cert-manager/components/yaml/crds.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.9.1/cert-manager.crds.yaml
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.10.0/cert-manager.crds.yaml
