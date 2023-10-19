ed018fd3c66bcb4ada4cb67e0c8201d1a9dfbb61
Arthur <arthur@arthurvardevanyan.com>
2023-10-19 19:46:58 -0400

    Terraform Pipeline Plan Incept

--- terraform/gcp/HomeLab/homelab/main.tf
+++ terraform/gcp/HomeLab/homelab/main.tf
@@ -5,8 +5,14 @@ terraform {
 
 provider "vault" {}
 
-provider "google" {}
-provider "google-beta" {}
+provider "google" {
+}
+
+terraform {
+  required_providers {
+    google = "= 4.84.0"
+  }
+}
 
 data "vault_generic_secret" "org" {
   path = "secret/gcp/org/av/projects"
@@ -35,4 +41,25 @@ resource "google_project_service" "secretmanager" {
   project            = "homelab-${local.project_id}"
   service            = "secretmanager.googleapis.com"
   disable_on_destroy = false
+}
+
+
+
+resource "google_service_account" "tekton" {
+  project      = "homelab-${local.project_id}"
+  account_id   = "tekton"
+  display_name = "tekton"
+}
+
+
+resource "google_project_iam_member" "tekton" {
+  project = "homelab-${local.project_id}"
+  role    = "roles/owner"
+  member  = google_service_account.tekton.member
+}
+
+resource "google_service_account_iam_member" "tekton" {
+  service_account_id = google_service_account.tekton.id
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principal://iam.googleapis.com/projects/${local.homelab_project_num}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.okd_homelab_wif.workload_identity_pool_id}/subject/system:serviceaccount:homelab:pipeline"
 }
