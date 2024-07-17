7b1972d51a1f0ad7bcc56338cc56d71f36ef93c8
Arthur Vardevanyan <arthur@arthurvardevanyan.com>
2024-07-16 23:49:17 -0400

    feat(Ceph Dashboarod): SSL + SAML Login (#82)

---
+++ terraform/homelab/zitadel_ceph.tf
@@ -0,0 +1,27 @@
+# ceph dashboard sso setup saml2 https://ceph.apps.okd.arthurvardevanyan.com https://zitadel.apps.okd.arthurvardevanyan.com/saml/v2/metadata UserName
+# ceph dashboard ac-user-create ArthurVardevanyan
+# ceph dashboard ac-user-create ArthurVardevanyan --force-password --enabled -i /tmp/pass
+# ceph dashboard ac-user-add-roles ArthurVardevanyan administrator
+resource "zitadel_project" "ceph" {
+  name                     = "ceph"
+  org_id                   = zitadel_org.zitadel.id
+  project_role_assertion   = false
+  project_role_check       = false
+  has_project_check        = false
+  private_labeling_setting = "PRIVATE_LABELING_SETTING_UNSPECIFIED"
+}
+
+provider "http" {
+}
+
+
+data "http" "get_ceph_saml" {
+  url = "https://ceph.apps.okd.arthurvardevanyan.com/auth/saml2/metadata"
+}
+
+resource "zitadel_application_saml" "ceph" {
+  org_id       = zitadel_org.zitadel.id
+  project_id   = zitadel_project.ceph.id
+  name         = "ceph"
+  metadata_xml = data.http.get_ceph_saml.response_body
+}
