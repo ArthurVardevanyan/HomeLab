839021525c8223d7df34a17249e70c76703777c3
Arthur <arthur@arthurvardevanyan.com>
2025-10-03 17:17:26 +0000

    feat(nextcloud): fpm s3 instance (#262)
    * feat(nextcloud): fpm s3 intance
    
    * feat(nextcloud): fpm s3 intance
    
    * feat(nextcloud): fpm s3 intance
    
    * feat(nextcloud): fpm s3 backup
    
    * feat(nextcloud): fpm folder refactor
    
    * feat(nextcloud): fpm network policy s3 update
    
    * feat(nextcloud): tune
    
    * feat(nextcloud): fpm network policy
    
    * feat(nextcloud): fpm pdb remove
    
    * feat(nextcloud): fpm egree firewall
    
    * feat(nextcloud): fpm unas backup
    
    * feat(nextcloud): fpm loose objects
    
    * feat(nextcloud): rsync decom
    
    * feat(nextcloud): fpm rclone fix
    
    * feat(nextcloud): rclone fast list
    
    * feat(nextcloud): fpm immich sync
    
    * feat(nextcloud): dragonfly resource increase
    
    * feat(nextcloud):  increase speed
    
    * feat(nextcloud):  s3 job tweak
    
    * feat(nextcloud):  fpm psql dump
    
    * feat(nextcloud):  fpm rclone checker tweak
    
    * feat(nextcloud):  fpm rclone, switch from copy to sync
    
    * feat(s3-ssd-unas):  incept
    
    * feat(nextcloud):  fpm db
    
    * feat(nextcloud): remove route on old instance
    
    * feat(nextcloud): tuning
    
    * feat(nextcloud): pg dump removal
    
    * feat(nextcloud): sync tune
    
    * feat(nextcloud): nginx tune
    
    * feat(nextcloud): immich pivot
    
    * feat(nextcloud): immich pivot
    
    * feat(nextcloud): folder delete

---
+++ kubernetes/nextcloud-fpm/components/backup-s3/s3.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir -p /tmp/.config/rclone
+cp /mnt/s3/rclone.conf /tmp/.config/rclone/rclone.conf
+rclone --progress sync ceph:nextcloud-8bc4b121-9fea-4f92-ac9c-2a1cce320084 unas-ssd:nextcloud --ignore-existing \
+--stats=10s --stats-log-level NOTICE --fast-list \
+--multi-thread-streams 8 --drive-chunk-size 128M --max-backlog 999999 \
+--transfers=25 --checkers=100 --buffer-size=75M
