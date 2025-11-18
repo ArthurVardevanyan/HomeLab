4da755af5cf9616c4be267a67a0162339cf499f5
Arthur Vardevanyan <arthur@arthurvardevanyan.com>
2025-11-18 18:51:11 -0500

    feat(stackrox): oidc conversion (#293)

---
+++ terraform/homelab/zitadel_stackrox.tf
@@ -0,0 +1,49 @@
+resource "zitadel_project" "stackrox" {
+  name                     = "stackrox"
+  org_id                   = zitadel_org.zitadel.id
+  project_role_assertion   = false
+  project_role_check       = false
+  has_project_check        = false
+  private_labeling_setting = "PRIVATE_LABELING_SETTING_UNSPECIFIED"
+}
+
+
+resource "zitadel_application_oidc" "stackrox" {
+  project_id = zitadel_project.stackrox.id
+  org_id     = zitadel_org.zitadel.id
+
+  name = "stackrox"
+  redirect_uris = [
+    "https://central-stackrox.app.okd.homelab.arthurvardevanyan.com/auth/response/oidc",
+    "https://central-stackrox.app.okd.homelab.arthurvardevanyan.com/sso/providers/oidc/callback"
+  ]
+  response_types              = ["OIDC_RESPONSE_TYPE_CODE"]
+  grant_types                 = ["OIDC_GRANT_TYPE_AUTHORIZATION_CODE"]
+  post_logout_redirect_uris   = []
+  app_type                    = "OIDC_APP_TYPE_WEB"
+  auth_method_type            = "OIDC_AUTH_METHOD_TYPE_BASIC"
+  version                     = "OIDC_VERSION_1_0"
+  clock_skew                  = "0s"
+  dev_mode                    = false
+  access_token_type           = "OIDC_TOKEN_TYPE_BEARER"
+  access_token_role_assertion = false
+  id_token_role_assertion     = false
+  id_token_userinfo_assertion = false
+  additional_origins          = []
+}
+
+
+
+resource "google_secret_manager_secret" "zitadel_stackrox" {
+  project   = "homelab-${local.project_id}"
+  secret_id = "zitadel_stackrox"
+
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_version" "zitadel_stackrox" {
+  secret      = google_secret_manager_secret.zitadel_stackrox.id
+  secret_data = zitadel_application_oidc.stackrox.client_secret
+}
