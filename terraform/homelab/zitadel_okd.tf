b64a110b6f9060efd9e95fe569d11d897941da45
Arthur Vardevanyan <arthur@arthurvardevanyan.com>
2026-08-21 22:56:26 -0400

    feat(auth): add OIDC CLI client with PKCE and separate ZITADEL app (#651)

--- terraform/homelab/zitadel_okd.tf
+++ terraform/homelab/zitadel_okd.tf
@@ -23,14 +23,9 @@ resource "zitadel_application_oidc" "okd" {
     "https://oauth-openshift.apps.okd.virt.arthurvardevanyan.com/oauth2callback/zitadel",
     "https://argocd.okd.virt.arthurvardevanyan.com/auth/callback",
     "https://argocd-apps.okd.virt.arthurvardevanyan.com/auth/callback",
-
-    "http://localhost:*"
   ]
-  response_types = ["OIDC_RESPONSE_TYPE_CODE"]
-  grant_types    = ["OIDC_GRANT_TYPE_AUTHORIZATION_CODE"]
-  post_logout_redirect_uris = [
-    "https://argocd.app.okd.homelab.arthurvardevanyan.com/auth/callback" # Can't remove this for some reason
-  ]
+  response_types              = ["OIDC_RESPONSE_TYPE_CODE"]
+  grant_types                 = ["OIDC_GRANT_TYPE_AUTHORIZATION_CODE"]
   app_type                    = "OIDC_APP_TYPE_WEB"
   auth_method_type            = "OIDC_AUTH_METHOD_TYPE_BASIC"
   version                     = "OIDC_VERSION_1_0"
@@ -57,4 +52,37 @@ resource "google_secret_manager_secret" "zitadel_okd" {
 resource "google_secret_manager_secret_version" "zitadel_okd" {
   secret      = google_secret_manager_secret.zitadel_okd.id
   secret_data = zitadel_application_oidc.okd.client_secret
+}
+
+
+resource "zitadel_application_oidc" "okd_cli" {
+  project_id = zitadel_project.okd.id
+  org_id     = zitadel_org.zitadel.id
+
+  name = "okd-cli"
+  redirect_uris = [
+    "http://localhost:*"
+  ]
+  response_types               = ["OIDC_RESPONSE_TYPE_CODE"]
+  grant_types                  = ["OIDC_GRANT_TYPE_AUTHORIZATION_CODE"]
+  app_type                     = "OIDC_APP_TYPE_NATIVE"
+  auth_method_type             = "OIDC_AUTH_METHOD_TYPE_NONE"
+  version                      = "OIDC_VERSION_1_0"
+  clock_skew                   = "0s"
+  dev_mode                     = true
+  access_token_type            = "OIDC_TOKEN_TYPE_BEARER"
+  access_token_role_assertion  = false
+  id_token_role_assertion      = false
+  id_token_userinfo_assertion  = true
+  additional_origins           = []
+  skip_native_app_success_page = true
+}
+
+resource "google_secret_manager_secret" "zitadel_okd_cli" {
+  project   = "homelab-${local.project_id}"
+  secret_id = "zitadel_okd_cli"
+
+  replication {
+    auto {}
+  }
 }
